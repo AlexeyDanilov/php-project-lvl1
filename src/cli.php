b8e635bcdd6328552d55781b423d28e396db0552
AlexeyDanilov <al.s.danilov@gmail.com>
2019-09-03 23:39:38 +0300

    add linter

--- src/cli.php
+++ src/cli.php
@@ -6,8 +6,9 @@ use function \cli\line;
 use function \cli\prompt;
 
 
-function run(){
- line("Welcome to the Brain Games!");
- $name = prompt('May I have your name?');
- line("Hello,%s", $name);
+function run()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt('May I have your name?');
+    line("Hello,%s", $name);
 }
