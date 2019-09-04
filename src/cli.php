693a1e728ec22fb96475bb7fecfb433e31162365
AlexeyDanilov <al.s.danilov@gmail.com>
2019-09-05 00:25:45 +0300

    add brain-even

--- src/cli.php
+++ src/cli.php
@@ -12,3 +12,36 @@ function run()
     $name = prompt('May I have your name?');
     line("Hello,%s", $name);
 }
+
+function even()
+{
+    line("Welcome to the Brain Games!");
+    line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+    $name = prompt('May I have your name?');
+    line("Hello,%s", $name);
+    $count = 0;
+    while ($count < 3) {
+        $num = rand(1, 100);
+        $answer = prompt("Question: {$num}");
+        if ($num % 2 === 0 && $answer === 'yes') {
+            line("Correct!");
+            $count++;
+        } elseif ($num % 2 !== 0 && $answer === 'no') {
+            line("Correct!");
+            $count++;
+        } elseif ($num % 2 === 0 && $answer === 'no') {
+            line("'no' is wrong answer ;(. Correct answer was 'yes'.
+                Let's try again, {$name}!");
+            break;
+        } elseif ($num % 2 !== 0 && $answer === 'yes') {
+            line("'yes' is wrong answer ;(. Correct answer was 'no'.
+                Let's try again, {$name}!");
+            break;
+        }
+    }
+    if ($count === 3) {
+        line("Congratulations, {$name}!");
+    } else {
+        return;
+    }
+}
